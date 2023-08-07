bce5cf625a891706bff3eb015077980e91c36a5e
JustAHacker <116762253+I-can-ddos@users.noreply.github.com>
2023-08-07 13:10:03 +0200

    main.py

--- main.py
+++ main.py
@@ -24,6 +24,6 @@ def main():
     print(f"    \"postal\": \"{ip_info['postal']}\",")
     print(f"    \"timezone\": \"{ip_info['timezone']}\"")
     print("}")
-
+    time.sleep(600)
 if __name__ == "__main__":
     main()
